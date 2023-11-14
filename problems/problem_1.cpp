b74671a584b20f9ba056f805db861082544965cc
IsmoilovMusoxon <147370659+IsmoilovMusoxon@users.noreply.github.com>
2023-11-14 14:43:04 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,33 +1,14 @@
-void problemSolution1(float consumed_water) {
-    float cost = 13;
-    if (consumed_water >= 30){
-        cost += 30*0.4;
-        consumed_water -=30;
+float problemSolution1(float consumed_water) {
+    float cost;
+    cost= 13 + consumed_water * 0.4;
+    if(consumed_water>30 && consumed_water<= 50){
+        cost = 13 + 30 * 0.4 + (consumed_water-30) * 0.12;
     }
-    else{
-        cost += consumed_water * 0.4;
-        consumed_water = 0;
+    else if (consumed_water>50 && consumed_water<=60){
+        cost = 13 + 30 * 0.4 + 20 * 0.12 + (consumed_water-50) *0.4;
     }
-
-    if (consumed_water >= 20){
-        cost += 20*0.12;
-        consumed_water -= 20;
-    }
-    else{
-        cost += consumed_water*0.12;
-        consumed_water = 0;
-    }
-
-    if (consumed_water >= 10){
-        cost += consumed_water * 1.4;
-        consumed_water -= 10;
-    }
-    else{
-        cost += consumed_water*1.4;
-        consumed_water = 0;
-    }
-    if (consumed_water != 0){
-        cost = consumed_water * 1.5;
+    else if (consumed_water>60){
+        cost = 13 + 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water - 60) * 1.5;
     }
     return cost;
 }
