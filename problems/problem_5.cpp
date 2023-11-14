f5e7d3499de2827f0261ab6fa264939e7539e846
IsmoilovMusoxon <147370659+IsmoilovMusoxon@users.noreply.github.com>
2023-11-14 14:08:14 +0500

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,16 +1,4 @@
 float problemSolution5(float x, float y, char operation) {
-switch (operation) {
-        case '+':
-            result = x+y;
-            break;
-        case '-':
-            result = x-y
-            break;
-        case '*':
-            result = x*y
-            break;
-        case '/':
-            result = x/y
-    }
+
    return result;
 }
