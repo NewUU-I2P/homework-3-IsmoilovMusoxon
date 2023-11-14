90fe1b0671c381b5e35a1c4154f129f23fe66cff
IsmoilovMusoxon <147370659+IsmoilovMusoxon@users.noreply.github.com>
2023-11-14 13:54:40 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,6 +1,43 @@
 void problemSolution1(float consumed_water) {
     float cost;
-    // write your code here
+#include <iostream>
+using namespace std;
+
+int main() {
+    const double fixedAmount = 13.0;
+    const double rate1 = 0.4;
+    const double rate2 = 0.12;
+    const double rate3 = 1.4;
+    const double additionalRate = 1.5;
+    double consumption;
+    cin >> consumption;
+    double totalCost = fixedAmount;
+    if (consumption > 30) {
+        totalCost += 30 * rate1;
+        consumption -= 30;
+
+        if (consumption > 20) {
+            totalCost += 20 * rate2;
+            consumption -= 20;
+
+            if (consumption > 10) {
+                totalCost += 10 * rate3;
+                consumption -= 10;
+
+                totalCost += consumption * additionalRate;
+            } else {
+                totalCost += consumption * rate3;
+            }
+        } else {
+            totalCost += consumption * rate2;
+        }
+    } else {
+        totalCost += consumption * rate1;
+    }
+    cout << totalCost << endl;
+
+    return 0;
+}
 
     return cost;
 }
